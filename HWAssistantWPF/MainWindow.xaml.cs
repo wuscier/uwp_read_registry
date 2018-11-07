0fc7d33b9ece8f211f916af8bf25d6d3df30985b
wuscier <1352288123@qq.com>
2018-11-08 00:06:27 +0800

    call ShellExecuteEx with C#

--- HWAssistantWPF/MainWindow.xaml.cs
+++ HWAssistantWPF/MainWindow.xaml.cs
@@ -29,9 +29,50 @@ namespace HWAssistantWPF
         [DllImport("HWASDKAgent.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
         internal static extern bool IsRegistryValueExist(UInt32 root, string subKey, string valueName);
 
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        static extern int ShellExecuteEx(ref SHELLEXECUTEINFO lpExecInfo);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SHELLEXECUTEINFO
+        {
+            public int cbSize;
+            public uint fMask;
+            public IntPtr hwnd;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpVerb;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpFile;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpParameters;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpDirectory;
+            public int nShow;
+            public IntPtr hInstApp;
+            public IntPtr lpIDList;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpClass;
+            public IntPtr hkeyClass;
+            public uint dwHotKey;
+            public IntPtr hIcon;
+            public IntPtr hProcess;
+        }
+
         private void btn_Click(object sender, RoutedEventArgs e)
         {
-            bool result = IsRegistryValueExist(0x80000002, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "SecurityHealth");
+            SHELLEXECUTEINFO info = new SHELLEXECUTEINFO();
+            info.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(info);
+            //info.lpVerb = "properties";
+            info.lpFile = "C:\\Windows\\System32\\cmd.exe";
+            info.nShow = 0;
+            //info.fMask = SEE_MASK_INVOKEIDLIST;
+
+            int result = -1;
+            result = ShellExecuteEx(ref info);
+            //int error = Marshal.GetLastWin32Error();
+            //Marshal.
+
+            //bool result = IsRegistryValueExist(0x80000002, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "SecurityHealth");
         }
     }
 }
